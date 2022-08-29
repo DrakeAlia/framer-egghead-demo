c9d7709110ce93c7fc4357ae3ef8330ee57d5833
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-08-29 13:52:52 -0700

    setup components and files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import ShoppingList from "./components/ShoppingList";
+import Bag from "./components/Bag";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Bag />
+      <div className="list-app">
+        <ShoppingList />
+      </div>
+    </>
   );
 }
-
-export default App;
