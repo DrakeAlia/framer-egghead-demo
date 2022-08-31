7016e727a87ffb7bae391f208f8e2bdb3d018c6f
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-08-30 19:37:59 -0700

    fix minor snytax for strokeLinecap, strokeLinejoin, and className in Bag component

--- src/components/Bag.js
+++ src/components/Bag.js
@@ -23,16 +23,16 @@ function Bag() {
         <div className="bag-svg">
             <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6"
+                className="h-6 w-6"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="#fff"
             >
             {/* add motion to path */}
                 <motion.path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="1"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="1"
                     d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
                     // add our variant prop with the variant we want
                     variants={svgVariants}
